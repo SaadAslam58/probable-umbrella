b5eda5e41f5ec9dc376235eeb7801962708466e1
SaadAslam58 <161113973+SaadAslam58@users.noreply.github.com>
2024-02-27 19:26:29 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+{
+    "Question#01";
+}
+console.log('hello asad} ,would you like to learn some python today');
+'Question#2';
+console.log('Person name=Asad} ,Uppercase:Asad,Lowercase:asad,Titlecase:Asad');
+'Question#3';
+console.log('Allama Iqbal} ,"Quotes"=Destiny is the prison and chain of the ignorant.');
+'Question#4';
+var famous_person = 'Allama Iqbal';
+var message = '{Allama Iqbal} once said, Destiny is the prison and chain of the ignorant.';
+console.log(message);
+'Question#5';
+var nam = '\t \n  Asad \t\n';
+console.log('Asad:');
+console.log('Asad');
+'Question#6';
+var strippedname = ('Asad');
+console.log('\n Asad:');
+console.log(strippedname);
+'Question#7';
+console.log('5x3');
+console.log('5+3');
+console.log('11-3');
+'Question#8';
+var favouritenoumber = 8;
+var messages = 'My favourite number is{8}.';
+console.log('message');
+var Description = ('messages} ,i like too send messages to my friend');
+'Question#9';
+var names = ["Saad", "Waleed"];
+for (var S = 0; S < names.length; S++) {
+    console.log(names[S]);
+}
+;
+for (var W = 0; W < names.length; W++) {
+    console.log(names[W]);
+}
+;
+'Question#10';
+var nams = ['Saad', 'Waleed', 'Asad'];
+function printmessages(names) {
+    names.forEach(function (names) {
+        console.log('Hello, {Waleed,Saad,Asad}! Have a great day!');
+    });
+}
---
+++ main.ts
@@ -0,0 +1,45 @@
+{"Question#01"}
+console.log('hello asad} ,would you like to learn some python today')
+'Question#2'
+console.log('Person name=Asad} ,Uppercase:Asad,Lowercase:asad,Titlecase:Asad')
+'Question#3'
+console.log('Allama Iqbal} ,"Quotes"=Destiny is the prison and chain of the ignorant.')
+'Question#4'
+let famous_person: string = 'Allama Iqbal';
+let message: string='{Allama Iqbal} once said, Destiny is the prison and chain of the ignorant.'
+console.log(message)
+'Question#5'
+let nam: string = '\t \n  Asad \t\n';
+console.log('Asad:');
+console.log('Asad')
+'Question#6'
+let strippedname: string = ('Asad');
+console.log('\n Asad:');
+console.log(strippedname)
+'Question#7'
+console.log('5x3')
+console.log('5+3')
+console.log('11-3')
+'Question#8'
+let favouritenoumber: number=8;
+let messages:string='My favourite number is{8}.';
+console.log('message');
+let Description : string =('messages} ,i like too send messages to my friend');
+'Question#9'
+let names: string[] =["Saad" , "Waleed"]
+for (let S = 0; S <names.length; S++)
+{console.log(names[S])};
+for (let W = 0; W <names.length; W++)
+{console.log(names[W])};
+'Question#10'
+let nams: string[] = ['Saad','Waleed','Asad'];
+function printmessages (names: string[]): void {names.forEach(names =>{
+    console.log('Hello, {Waleed,Saad,Asad}! Have a great day!');
+})
+}
+
+
+
+
+
+
